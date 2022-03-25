8e9bff1e1c58308d9309b3276d539b1e683ca29c
U072030 <nur_kolilik@bca.co.id>
2022-03-25 15:19:04 +0700

    Creating the Spring MVC Controller

---
+++ src/main/java/kolilik/springframework/spring5jokesapp/controllers/JokeController.java
@@ -0,0 +1,21 @@
+package kolilik.springframework.spring5jokesapp.controllers;
+
+import kolilik.springframework.spring5jokesapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private final JokeService jokeService;
+
+    public JokeController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model){
+        model.addAttribute("Joke", jokeService.getJoke());
+        return "index";
+    }
+}
